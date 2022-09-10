b5f31b01d0a791765702eae53a7451bf803171c3
cheesechoux <gsoosuu@gmail.com>
2022-09-11 01:02:23 +0900

    Create: controllers

---
+++ controllers/shop.js
@@ -0,0 +1,52 @@
+const Product = require('../models/product');
+
+exports.getProducts = (req, res, next) => {
+  Product.findAll()
+    .then(products => {
+      res.render('shop/product-list', {
+        prods: products,
+        pageTitle: 'All Products',
+        path: '/products'
+      });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
+
+// findAll, findByPk 둘 다 사용 가능
+exports.getProduct = (req, res, next) => {
+  const prodId = req.params.productId;
+  // Product.findAll({where: { id: prodId } }) 
+  //   .then(products => {
+  //     res.render('shop/product-detail', {
+  //       product: products[0], // findAll 함수는 디폴트로 배열을 줌 0으로 잡아줘야함
+  //       pageTitle: products[0].title,
+  //       path: '/products'
+  //     });
+  //   })
+  //   .catch(err => console.log(err));
+  Product.findByPk(prodId) 
+    .then(product => {
+      res.render('shop/product-detail', {
+        product: product,
+        pageTitle: product.title,
+        path: '/products'
+      });
+    })
+    .catch(err => console.log(err));
+};
+
+exports.getIndex = (req, res, next) => {
+  Product.findAll()
+    .then(products => {
+      res.render('shop/index', {
+        prods: products,
+        pageTitle: 'Shop',
+        path: '/'
+      });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
