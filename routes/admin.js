90c9410fd0d60db4d5d30e922e41f522c64ea9f7
cheesechoux <gsoosuu@gmail.com>
2022-10-06 21:20:42 +0900

    Create: protectRoutes

--- routes/admin.js
+++ routes/admin.js
@@ -3,22 +3,23 @@ const path = require('path');
 const express = require('express');
 
 const adminController = require('../controllers/admin');
+const isAuth = require('../middleware/is-auth');
 
 const router = express.Router();
 
 // /admin/add-petsitter => GET
-router.get('/add-petsitter', adminController.getAddPetsitter);
+router.get('/add-petsitter', isAuth, adminController.getAddPetsitter);
 
 // /admin/petsitters => GET
-router.get('/petsitters', adminController.getPetsitters);
+router.get('/petsitters', isAuth, adminController.getPetsitters);
 
 // /admin/add-petsitter => POST
-router.post('/add-petsitter', adminController.postAddPetsitter);
+router.post('/add-petsitter', isAuth, adminController.postAddPetsitter);
 
-router.get('/edit-product/:petsitterId', adminController.getEditPetsitter);
+router.get('/edit-product/:petsitterId', isAuth, adminController.getEditPetsitter);
 
-router.post('/edit-petsitter', adminController.postEditPetsitter);
+router.post('/edit-petsitter', isAuth, adminController.postEditPetsitter);
 
-router.post('/delete-petsitter', adminController.postDeletePetsitter);
+router.post('/delete-petsitter', isAuth, adminController.postDeletePetsitter);
 
 module.exports = router;
